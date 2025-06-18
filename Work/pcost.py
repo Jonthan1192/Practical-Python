a2290023fd46a893abade95956ace42670c9116f
Jonathan Amarin <amarin.jonathan123@gmail.com>
2025-06-18 17:54:17 +0300

    Part 5

--- Work/pcost.py
+++ Work/pcost.py
@@ -9,9 +9,7 @@ from stock import Stock
 
 def portfolio_cost(filename: Path) -> float:
     portfolio = read_portfolio(filename)
-    total_cost = 0.0
-    for stock in portfolio:
-        total_cost += stock.shares * stock.price
+    total_cost = sum([stock.cost for stock in portfolio])
     return total_cost
 
 
--- Work/stock.py
+++ Work/stock.py
@@ -1,11 +1,24 @@
 # stock.py
 
 class Stock:
+    __slots__ = ('name', '_shares', 'price')
+
     def __init__(self, name: str, shares: int, price: float):
         self.name = name
         self.shares = shares
         self.price = price
 
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected int')
+        self._shares = value
+
+    @property
     def cost(self):
         return self.shares * self.price
 
